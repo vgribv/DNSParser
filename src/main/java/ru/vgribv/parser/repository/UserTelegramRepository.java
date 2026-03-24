c5f47976b2a844ce0d819485909e63a58b95a1d9
vgribv <zGVDz@yandex.ru>
2026-03-24 17:30:46 +0300

    Refactor: Add specific query methods to UserTelegramRepository

--- src/main/java/ru/vgribv/parser/repository/UserTelegramRepository.java
+++ src/main/java/ru/vgribv/parser/repository/UserTelegramRepository.java
@@ -1,11 +1,18 @@
 package ru.vgribv.parser.repository;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.data.repository.CrudRepository;
 import ru.vgribv.parser.entity.UserTelegram;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserTelegramRepository extends CrudRepository<UserTelegram, Long> {
     @Override
+    @NonNull
     List<UserTelegram> findAll();
+
+    List<UserTelegram> findAllByIsActive(boolean isActive);
+
+    Optional<UserTelegram> findByChatId(long chatId);
 }
