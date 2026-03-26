f965d2b02e1e0c649f978fe01830515f24557fbb
vgribv <zGVDz@yandex.ru>
2026-03-26 17:30:36 +0300

    Refactor: Improved the file writing service.
    - File updates throughout the day
    - Clean up obsolete files to save disk space.

--- src/main/java/ru/vgribv/parser/service/FileWriteService.java
+++ src/main/java/ru/vgribv/parser/service/FileWriteService.java
@@ -1,65 +1,101 @@
 package ru.vgribv.parser.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.vgribv.parser.entity.Product;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.FileTime;
+import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Service
+@Slf4j
 public class FileWriteService {
-    public void writeFile(List<Product> bufferNew, List<Product> bufferPriceHasDecreased, List<Product> bufferRemoved) throws IOException {
 
-        Path directory = Paths.get("data");
-        if (!Files.exists(directory)) {
-            Files.createDirectories(directory);
-        }
+    public void writeFile(List<Product> bufferNew, List<Product> bufferPriceHasDecreased, List<Product> bufferRemoved) {
 
-        Path path = directory.resolve("goodsLog.txt");
+        String date = LocalDate.now().toString();
+        Path path = Paths.get("data", "report_" + date + ".csv");
 
-        StringBuilder sb = new StringBuilder();
-        sb.append("\n\n=== ОТЧЕТ ОТ ").append(LocalDateTime.now()
-                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))).append(" ===\n");
+        try {
+            Files.createDirectories(path.getParent());
+            boolean isNewFile = Files.notExists(path);
 
+            StringBuilder sb = new StringBuilder();
 
-        appendBuffer(sb, "НОВЫЕ ТОВАРЫ:", bufferNew);
-        appendBuffer(sb, "ПОНИЖЕНИЕ ЦЕНЫ:", bufferPriceHasDecreased);
-        appendBuffer(sb, "УДАЛЕННЫЕ ТОВАРЫ:", bufferRemoved);
-
-        Files.writeString(path, sb.toString(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
-    }
-
-    private void appendBuffer(StringBuilder sb, String title, List<Product> buffer) {
-        if (buffer != null && !buffer.isEmpty()) {
-            sb.append("\n").append(title).append("\n");
-
-            String line;
-            String link = "https://www.dns-shop.ru/catalog/markdown/";
-
-            for (Product p : buffer) {
-                switch (title){
-                    case "НОВЫЕ ТОВАРЫ" ->
-                        line = p.getName() + " | " + p.getDiscountPrice() + "руб./" + p.getFullPrice() + "руб. " +
-                                "категория:" + p.getCategory() + " | " + link + p.getId() + "\n";
-
-                    case "ПОНИЖЕНИЕ ЦЕНЫ" ->
-                        line = p.getName() + " | новая цена: " + p.getDiscountPrice() + "руб. | старая цена: " +
-                                p.getOldDiscountPrice() + " | " + link + p.getId() + "\n";
-
-                    case "УДАЛЕННЫЕ ТОВАРЫ" ->
-                        line = "Купили: " + p.getName() + " за " + p.getDiscountPrice() + "руб." + "\n";
-                    default ->
-                            line = p.getName();
-                }
-                sb.append("- ").append(line).append("\n");
+            if (isNewFile) {
+                sb.append("Тип изменения;Название;Новая цена;Старая цена;Ссылка\n");
             }
 
+            String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm"));
+
+            appendBuffer(sb, timestamp, "НОВАЯ ПОЗИЦИЯ", bufferNew);
+            appendBuffer(sb, timestamp, "СКИДКА", bufferPriceHasDecreased);
+            appendBuffer(sb, timestamp, "ПРОДАНО", bufferRemoved);
+
+            Files.writeString(path, sb.toString(), Charset.forName("Windows-1251"),
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.TRUNCATE_EXISTING);
+
+            log.info("Отчет успешно обновлен: {}", path);
+
+            deleteOldReports();
+        } catch (IOException e) {
+            log.error("Ошибка в блоке записи отчета: ", e);
+        }
+    }
+
+    private void appendBuffer(StringBuilder sb, String timestamp, String type, List<Product> buffer) {
+        if (buffer == null || buffer.isEmpty()) return;
+
+        String linkPrefix = "https://www.dns-shop.ru";
+
+        for (Product p : buffer) {
+            sb.append(timestamp)
+                    .append(type).append(";")
+                    .append(p.getName().replace(";", " ")).append(";")
+                    .append(p.getDiscountPrice()).append(";")
+                    .append(p.getOldDiscountPrice() != null ? p.getOldDiscountPrice() : "").append(";")
+                    .append(linkPrefix).append(p.getLinkId()).append("\n");
+        }
+    }
+
+    private void deleteOldReports() {
+        Path directory = Paths.get("data");
+        int daysLimit = 7;
+        Instant threshold = Instant.now().minus(daysLimit, ChronoUnit.DAYS);
+
+        try (Stream<Path> files = Files.list(directory)) {
+            files.filter(path -> path.toString().endsWith(".csv")).filter(path -> {
+                        try {
+                            FileTime lastModified = Files.getLastModifiedTime(path);
+                            return lastModified.toInstant().isBefore(threshold);
+                        } catch (IOException e) {
+                            return false;
+                        }
+                    })
+                    .forEach(path -> {
+                        try {
+                            Files.delete(path);
+                            log.info("Удален старый отчет: {}", path.getFileName());
+                        } catch (IOException e) {
+                            log.error("Ну удалось удалить файл: {}", path);
+                        }
+                    });
+        } catch (IOException e) {
+            log.error("Ошибка при очистке старых отчетов: ", e);
         }
     }
 }
