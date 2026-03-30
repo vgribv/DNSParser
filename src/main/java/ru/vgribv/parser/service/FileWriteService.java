c2af512fffac86cf2c898d01001ff504b1747a3c
vgribv <zGVDz@yandex.ru>
2026-03-30 18:36:35 +0300

    refactor: decompose FileWriteService into modular methods and add Excel BOM support

--- src/main/java/ru/vgribv/parser/service/FileWriteService.java
+++ src/main/java/ru/vgribv/parser/service/FileWriteService.java
@@ -1,12 +1,13 @@
 package ru.vgribv.parser.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.vgribv.parser.entity.Product;
+import ru.vgribv.parser.repository.ProductRepository;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -24,7 +25,76 @@ import java.util.stream.Stream;
 @Slf4j
 public class FileWriteService {
 
-    public void writeFile(List<Product> bufferNew, List<Product> bufferPriceHasDecreased, List<Product> bufferRemoved) {
+    private final ProductRepository productRepository;
+    private final String linkPrefix;
+
+    public FileWriteService(ProductRepository productRepository, @Value("${dns.link.prefix}") String linkPrefix) {
+        this.productRepository = productRepository;
+        this.linkPrefix = linkPrefix;
+    }
+
+    public void updateAllReports(List<Product> bufferNew, List<Product> bufferPriceHasDecreased, List<Product> bufferRemoved){
+        createReportFile(bufferNew, bufferPriceHasDecreased, bufferRemoved);
+        deleteOldReports();
+        createProductsFile();
+        createPercentReport();
+    }
+
+    private void createProductsFile() {
+        Path path = Paths.get("data", "productsReport.csv");
+        try {
+            Files.createDirectories(path.getParent());
+            StringBuilder sb = new StringBuilder();
+            sb.append("\uFEFF");
+            sb.append("Название;Скидочная цена;Полная цена;Категория;Ссылка\n");
+            for (Product product : productRepository.findAll()) {
+                sb.append(product.getName().replace(";", " ")).append(";")
+                        .append(product.getDiscountPrice()).append(";")
+                        .append(product.getFullPrice()).append(";")
+                        .append(product.getCategory().getName()).append(";")
+                        .append(linkPrefix).append(product.getLinkId()).append("\n");
+            }
+            Files.writeString(path, sb.toString(), StandardCharsets.UTF_8,
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.TRUNCATE_EXISTING);
+
+            logInfo(path);
+        } catch (IOException e) {
+            log.error("Ошибка в блоке записи отчета: ", e);
+        }
+    }
+
+    private void createPercentReport() {
+        Path path = Paths.get("data", "productsPercentReport.csv");
+
+        try {
+            Files.createDirectories(path.getParent());
+            StringBuilder sb = new StringBuilder();
+            sb.append("\uFEFF");
+            sb.append("Скидка %;Название;Скидочная цена;Полная цена;Категория;Ссылка\n");
+
+            for(Product product : productRepository.findAllOrderByDiscountPercentDesc()){
+                int percent = (product.getFullPrice() - product.getDiscountPrice()) * 100 / product.getFullPrice();
+
+                sb.append(percent).append("%;")
+                        .append(product.getName().replace(";", " ")).append(";")
+                        .append(product.getDiscountPrice()).append(";")
+                        .append(product.getFullPrice()).append(";")
+                        .append(product.getCategory().getName()).append(";")
+                        .append(linkPrefix).append(product.getLinkId()).append("\n");
+            }
+
+            Files.writeString(path, sb.toString(), StandardCharsets.UTF_8,
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.TRUNCATE_EXISTING);
+
+            logInfo(path);
+        } catch (IOException e) {
+            log.error("Ошибка в блоке записи отчета: ", e);
+        }
+    }
+
+    private void createReportFile(List<Product> bufferNew, List<Product> bufferPriceHasDecreased, List<Product> bufferRemoved) {
 
         String date = LocalDate.now().toString();
         Path path = Paths.get("data", "report_" + date + ".csv");
@@ -36,22 +106,21 @@ public class FileWriteService {
             StringBuilder sb = new StringBuilder();
 
             if (isNewFile) {
-                sb.append("Тип изменения;Название;Новая цена;Старая цена;Ссылка\n");
+                sb.append("\uFEFF");
+                sb.append("Время;Тип изменения;Название;Новая цена;Старая цена;Полная цена;Ссылка\n");
             }
 
             String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm"));
 
-            appendBuffer(sb, timestamp, "НОВАЯ ПОЗИЦИЯ", bufferNew);
-            appendBuffer(sb, timestamp, "СКИДКА", bufferPriceHasDecreased);
-            appendBuffer(sb, timestamp, "ПРОДАНО", bufferRemoved);
+            appendBuffer(sb, timestamp, "Новый товар", bufferNew);
+            appendBuffer(sb, timestamp, "Понижение цены", bufferPriceHasDecreased);
+            appendBuffer(sb, timestamp, "Продано", bufferRemoved);
 
-            Files.writeString(path, sb.toString(), Charset.forName("Windows-1251"),
+            Files.writeString(path, sb.toString(), StandardCharsets.UTF_8,
                     StandardOpenOption.CREATE,
-                    StandardOpenOption.TRUNCATE_EXISTING);
+                    StandardOpenOption.APPEND);
 
-            log.info("Отчет успешно обновлен: {}", path);
-
-            deleteOldReports();
+            logInfo(path);
         } catch (IOException e) {
             log.error("Ошибка в блоке записи отчета: ", e);
         }
@@ -60,15 +129,18 @@ public class FileWriteService {
     private void appendBuffer(StringBuilder sb, String timestamp, String type, List<Product> buffer) {
         if (buffer == null || buffer.isEmpty()) return;
 
-        String linkPrefix = "https://www.dns-shop.ru";
-
-        for (Product p : buffer) {
-            sb.append(timestamp)
+        for (Product product : buffer) {
+            Integer current = product.getDiscountPrice();
+            Integer old = product.getOldDiscountPrice();
+            Integer full = product.getFullPrice();
+            boolean isChanged = old != null && !old.equals(current);
+            sb.append(timestamp).append(";")
                     .append(type).append(";")
-                    .append(p.getName().replace(";", " ")).append(";")
-                    .append(p.getDiscountPrice()).append(";")
-                    .append(p.getOldDiscountPrice() != null ? p.getOldDiscountPrice() : "").append(";")
-                    .append(linkPrefix).append(p.getLinkId()).append("\n");
+                    .append(product.getName().replace(";", " ")).append(";")
+                    .append(isChanged ? current : "").append(";")
+                    .append(isChanged ? old : current).append(";")
+                    .append(full != null ? full : "").append(";")
+                    .append(linkPrefix).append(product.getLinkId()).append("\n");
         }
     }
 
@@ -78,7 +150,7 @@ public class FileWriteService {
         Instant threshold = Instant.now().minus(daysLimit, ChronoUnit.DAYS);
 
         try (Stream<Path> files = Files.list(directory)) {
-            files.filter(path -> path.toString().endsWith(".csv")).filter(path -> {
+            files.filter(path -> path.getFileName().toString().startsWith("report_")).filter(path -> {
                         try {
                             FileTime lastModified = Files.getLastModifiedTime(path);
                             return lastModified.toInstant().isBefore(threshold);
@@ -97,5 +169,9 @@ public class FileWriteService {
         } catch (IOException e) {
             log.error("Ошибка при очистке старых отчетов: ", e);
         }
+    }
+
+    private void logInfo(Path path) {
+        log.info("Отчет успешно обновлен: {}", path);
     }
 }
