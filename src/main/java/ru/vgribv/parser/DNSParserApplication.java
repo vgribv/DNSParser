80146a7140d428ffdffd889d47a0bd384affcc1f
vgribv <zGVDz@yandex.ru>
2026-04-03 15:52:29 +0300

    fix: global proxy and host networking stability

--- src/main/java/ru/vgribv/parser/DNSParserApplication.java
+++ src/main/java/ru/vgribv/parser/DNSParserApplication.java
@@ -15,6 +15,10 @@ import ru.vgribv.parser.config.TelegramBotConfig;
 @Import(TelegramBotConfig.class)
 public class DNSParserApplication {
 	public static void main(String[] args) {
+		System.setProperty("http.proxyHost", "127.0.0.1");
+		System.setProperty("http.proxyPort", "1111");
+		System.setProperty("https.proxyHost", "127.0.0.1");
+		System.setProperty("https.proxyPort", "1111");
 		SpringApplication.run(DNSParserApplication.class, args);
 	}
 }
